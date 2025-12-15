80c9cc26e8b84883e8f98ba092b46ec5761e5070
jhonnyxjj <gjhonnybr@gmail.com>
2025-12-15 16:26:03 -0300

    :sparkles: (file-upload) create upload list component

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -3,7 +3,7 @@ import { FileUpload } from "./components/file-upload";
 export function App() {
 
   return (
-     <main className="h-dvh flex flex-col items-center justify-center p-10">
+     <main className="h-dvh flex flex-col items-center justify-center p-2 sm:p-10">
       <FileUpload />
     </main>
     
