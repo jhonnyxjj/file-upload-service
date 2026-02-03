a4835ac8fd6d4f492290a3d25abdd50822bcab16
Jhonnyxjj <gjhonnybr@gmail.com>
2026-02-02 22:06:55 -0300

    feat: add chunkSizeWarningLimit to have more space in the build

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, 
+  },
   plugins: [
     react(),
     tailwindcss()
